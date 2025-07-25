22a2a7a9467efb0c2d3fb27a7651655f0b6e67b4
A-Shushi <219606551+A-Shushi@users.noreply.github.com>
2025-07-25 11:14:12 +0700

    Complete the logic for the 5 round game with results
    Create a div as a title to hold the round number
    Use the round number to determine whether a game should be played or not
    Use the round number to disable buttons when the game has ended
    Move commented out old code to the bottom of the script for future reference

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+// Declare constants that contain DOM elements
+const roundTitle = document.querySelector("#round")
 const gameboard = document.querySelector(".gameboard")
 const matchResult = document.querySelector("#match-result")
 const scoreBoard = document.querySelector("#score")
+const buttonNodeList = gameboard.querySelectorAll("button")
 
 // Define getComputerChoice function where a computer randomly chooses either rock, paper or scissors
 function getComputerChoice() {
@@ -15,6 +18,76 @@ function getComputerChoice() {
     }
 }
 
+// Declare humanScore, computerScore, roundNumber variables that keep score of the game
+
+let humanScore = 0;
+let computerScore = 0;
+let roundNumber = 1
+
+// Logic to play a single round of the game
+
+function playRound(clickedChoice) {
+    if (roundNumber <= 5) {
+        let computerChoice = getComputerChoice();
+        let humanChoice = clickedChoice;
+        if (computerChoice === "Rock" && humanChoice === "Rock") {
+            matchResult.textContent = "It's a draw! Rock vs Rock";
+        } else if (computerChoice === "Rock" && humanChoice === "Paper") {
+            humanScore += 1;
+            matchResult.textContent = "You win! Paper beats Rock!";
+        } else if (computerChoice === "Rock" && humanChoice === "Scissors") {
+            computerScore += 1;
+            matchResult.textContent = "You lose! Rock beats Scissors!";
+        } else if (computerChoice === "Paper" && humanChoice === "Rock") {
+            computerScore += 1;
+            matchResult.textContent = "You lose! Paper beats Rock!";
+        } else if (computerChoice === "Paper" && humanChoice === "Paper") {
+            matchResult.textContent = "It's a draw! Paper vs Paper";
+        } else if (computerChoice === "Paper" && humanChoice === "Scissors") {
+            humanScore += 1;
+            matchResult.textContent = "You win! Scissors beat Paper!";
+        } else if (computerChoice === "Scissors" && humanChoice === "Rock") {
+            humanScore += 1;
+            matchResult.textContent = "You win! Rock beats Scissors!";
+        } else if (computerChoice === "Scissors" && humanChoice === "Paper") {
+            computerScore += 1;
+            matchResult.textContent = "You lose! Scissors beat Paper!";
+        } else if (computerChoice === "Scissors" && humanChoice === "Scissors") {
+            matchResult.textContent = "It's a draw! Scissors vs Scissors";
+        }
+        scoreBoard.textContent = `Your score: ${humanScore} | Computer Score: ${computerScore}`;
+        roundNumber += 1;
+        roundTitle.textContent = `Round ${roundNumber}!`
+    }
+}
+
+roundTitle.textContent = `Welcome to a game of Rock, Paper Scissors! Round ${roundNumber}!`;
+
+gameboard.addEventListener("click", (event) => {
+    let humanChoice = event.target.id;
+    if (roundNumber < 5) {
+        playRound(humanChoice);
+    } else {
+        playRound(humanChoice);
+        roundTitle.textContent = "GAME OVER!"
+        if (humanScore > computerScore) {
+            matchResult.textContent = "Congratulation, you WIN! To play again, hit the refresh button!";
+        } else if (humanScore < computerScore) {
+            matchResult.textContent = "Better luck next time, you LOST! To play again, hit the refresh button!";
+        } else {
+            matchResult.textContent = "It's a DRAW! To play again, hit the refresh button!";
+        }
+        buttonNodeList.forEach((button) => {
+            button.disabled = true;
+        })
+    }
+})
+
+
+
+
+// OLD CODE
+
 // Define getHumanChoice function where a human enters their choice and program returns it
 
 // function getHumanChoice() {
@@ -23,58 +96,6 @@ function getComputerChoice() {
 //     return lowerCaseChoice.charAt(0).toUpperCase() + lowerCaseChoice.slice(1);  // Convert string to only have first capital letter
 // }
 
-// Declare humanScore computerScore variables that keep score of the game
-
-let humanScore = 0;
-let computerScore = 0;
-
-// Logic to play a single round of the game
-
-function playRound(clickedChoice) {
-    let computerChoice = getComputerChoice();
-    let humanChoice = clickedChoice;
-    if (computerChoice === "Rock" && humanChoice === "Rock") {
-        matchResult.textContent = "It's a draw! Rock vs Rock";
-        scoreBoard.textContent = `Your score: ${humanScore} | Computer Score: ${computerScore}`
-    } else if (computerChoice === "Rock" && humanChoice === "Paper") {
-        humanScore += 1;
-        matchResult.textContent = "You win! Paper beats Rock!";
-        scoreBoard.textContent = `Your score: ${humanScore} | Computer Score: ${computerScore}`
-    } else if (computerChoice === "Rock" && humanChoice === "Scissors") {
-        computerScore += 1;
-        matchResult.textContent = "You lose! Rock beats Scissors!";
-        scoreBoard.textContent = `Your score: ${humanScore} | Computer Score: ${computerScore}`
-    } else if (computerChoice === "Paper" && humanChoice === "Rock") {
-        computerScore += 1;
-        matchResult.textContent = "You lose! Paper beats Rock!";
-        scoreBoard.textContent = `Your score: ${humanScore} | Computer Score: ${computerScore}`
-    } else if (computerChoice === "Paper" && humanChoice === "Paper") {
-        matchResult.textContent = "It's a draw! Paper vs Paper";
-        scoreBoard.textContent = `Your score: ${humanScore} | Computer Score: ${computerScore}`
-    } else if (computerChoice === "Paper" && humanChoice === "Scissors") {
-        humanScore += 1;
-        matchResult.textContent = "You win! Scissors beat Paper!";
-        scoreBoard.textContent = `Your score: ${humanScore} | Computer Score: ${computerScore}`
-    } else if (computerChoice === "Scissors" && humanChoice === "Rock") {
-        humanScore += 1;
-        matchResult.textContent = "You win! Rock beats Scissors!";
-        scoreBoard.textContent = `Your score: ${humanScore} | Computer Score: ${computerScore}`
-    } else if (computerChoice === "Scissors" && humanChoice === "Paper") {
-        computerScore += 1;
-        matchResult.textContent = "You lose! Scissors beat Paper!";
-        scoreBoard.textContent = `Your score: ${humanScore} | Computer Score: ${computerScore}`
-    } else if (computerChoice === "Scissors" && humanChoice === "Scissors") {
-        matchResult.textContent = "It's a draw! Scissors vs Scissors";
-        scoreBoard.textContent = `Your score: ${humanScore} | Computer Score: ${computerScore}`
-    }
-}
-
-gameboard.addEventListener("click", (event) => {
-    let humanChoice = event.target.id
-    playRound(humanChoice)
-})
-
-
 // Write the logic to play the game for 5 rounds
 
 // function playGame() {
@@ -82,13 +103,13 @@ gameboard.addEventListener("click", (event) => {
 //         playRound();
 //         console.log(`Your Score: ${humanScore}\nComputer Score: ${computerScore}`)
 //     }
-//     if (humanScore > computerScore) {
-//         console.log("Congratulation, you WIN! To play again, hit the refresh button!")
-//     } else if (humanScore < computerScore) {
-//         console.log("Better luck next time, you LOST! To play again, hit the refresh button!")
-//     } else {
-//         console.log("It's a DRAW! To play again, hit the refresh button!")
-//     }
+// if (humanScore > computerScore) {
+//     console.log("Congratulation, you WIN! To play again, hit the refresh button!")
+// } else if (humanScore < computerScore) {
+//     console.log("Better luck next time, you LOST! To play again, hit the refresh button!")
+// } else {
+//     console.log("It's a DRAW! To play again, hit the refresh button!")
+// }
 // }
 
 // playGame()
